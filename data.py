c0c112de6c4b31c308db43569667437a6fc752b8
Andrzej Darowski <11andar07@gmail.com>
2024-03-25 21:32:55 +0100

    Added initial data and recipient information to the configuration file.

---
+++ data.py
@@ -0,0 +1,15 @@
+# Enter your data here
+my_data = {
+    "email": "Enter your email here",
+    "app_password": "Enter your app password here",
+    "lat": 0.0,   # Enter latitude of your current location here (initially 0.0)
+    "lng": 0.0,   # Enter longitude of your current location here (initially 0.0)
+    "OW_API_key": "Enter your OpenWeather API key here",
+}
+
+# Enter recipients' names, surnames and email addresses
+RECIPIENTS = {
+    "Name Surname": {
+        "email": "Enter recipient's email here"
+    }
+}
